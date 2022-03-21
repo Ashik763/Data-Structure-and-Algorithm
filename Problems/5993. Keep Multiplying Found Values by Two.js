e455a03c5e6bf56059d3aafc7cdf7978a267be9a
Ashik Ghosh <ashikghosh763@gmail.com>
2022-03-21 23:52:51 +0600

    linked list

--- Problems/5993. Keep Multiplying Found Values by Two.js
+++ Problems/5993. Keep Multiplying Found Values by Two.js
@@ -1,14 +1,20 @@
 var findFinalValue = function(nums, original) {
 
-    let i;
-    for(i = 0; i < nums.length; i++) {
-        if(nums[i] == original){
-            original = 2 * original;
-            i=-1;
-        }
-    }
-    // console.log(original);
-    return original;
+    // let i,name;
+    // let s = {name:"Ashik",age:66};
+    // s['age'] = 33;
+    // console.log(s);
+    // for(i = 0; i < nums.length; i++) {
+    //     if(nums[i] == original){
+    //         original = 2 * original;
+    //         i=-1;
+    //     }
+    // }
+    // // console.log(original);
+    // return original;
+    const f = new FormData();
+    f.append('file',4);
+    console.log(f);
     
 };
 findFinalValue([8,19,4,2,15,3],2)
