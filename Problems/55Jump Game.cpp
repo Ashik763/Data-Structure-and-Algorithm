e6f1195dec533fc4e89f792c2cf4a238d0080fce
Ashik Ghosh <ashikghosh763@gmail.com>
2022-04-27 23:41:29 +0600

    jump game

---
+++ Problems/55Jump Game.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+      bool canJump(vector<int>&nums){
+      int target = nums.size()-1;
+      for(int i=target-1;i>0;i--)
+      {
+          if(i+nums[i] >= target){
+            target = i;
+          }
+
+      }
+       return  (nums[0] >= target);
+
+
+  }
+
+
+int main(){
+
+    vector<int> nums;
+    nums.push_back(3);
+    nums.push_back(1);
+    canJump(nums);
+
+}
