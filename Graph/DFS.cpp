e23a951da25d323ce9d6ef5c877b723a89f15b0d
Ashik Ghosh <ashikghosh763@gmail.com>
2023-02-21 03:00:37 +0600

    special day,started learning graph

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -21,13 +21,13 @@ vector<int> g[N];
 void dfs(int vertex){
 
 	vis[vertex]= true;
-	cout << vertex << endl;
+	
 	for(auto child:g[vertex]){
-		cout << "par" << vertex << " , child" << child << endl;
+		
 		if( vis[child]== true)continue;
 		dfs(child);
 	}
-	cout << "after finishing " << vertex << endl;
+	
 }
 
 
@@ -37,30 +37,17 @@ int main() {
     boost;
     int n,m;
     cin >> n >> m;
-    vector<int> p[5];
-    for(int i=0;i<5;i++){
-    	int a;
-    	cin >> a ;
-    	p[i].pb({a});
-    }
 
-    for(auto v:p){
-    	for(auto val:v){
-    		cout << val << ' ';
-    	}
-
-    	cout << endl;
-    }
     	
-    // while(m--){
-    // 	int a,b;
-    // 	cin >> a >> b;
-    // 	g[a].pb(b);
-    // 	g[b].pb(a);
-    // }
-    // dfs(1);
-    // }
-    // }
+    while(m--){
+    	int a,b;
+    	cin >> a >> b;
+    	g[a].pb(b);
+    	g[b].pb(a);
+    }
+    dfs(1);
+    
+    
      
 }
 
